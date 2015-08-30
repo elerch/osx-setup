3cf635f136ee74817d3b03131beae262c1125c74
Emil Lerch <emil@lerch.org>
2015-08-29 23:26:32 -0700

    add fonts

---
+++ scripts/fonts.sh
@@ -0,0 +1,14 @@
+# OS X Font Setup
+
+# Assumes: 
+# + Homebrew has been installed correctly.
+# + Homebrew-cask has been tapped and installed correctly.
+
+brew tap caskroom/fonts
+
+# Popular fonts
+brew cask install font-roboto
+brew cask install font-hack
+brew cask install font-inconsolata
+brew cask install font-source-code-pro
+
