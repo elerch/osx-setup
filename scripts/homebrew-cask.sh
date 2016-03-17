0796bfb13848759788b793ba72518a6d158cb3cc
Emil Lerch <emil@lerch.org>
2016-03-16 17:23:00 -0700

    change to current caskroom

--- scripts/homebrew-cask.sh
+++ scripts/homebrew-cask.sh
@@ -9,7 +9,7 @@
 # '/usr/local/'
 # brew-cask Casks (packages) stored in /opt/homebrew-cask/Caskroom and they are
 # symlinked to ${HOME}/Applications as *.app executables.
-brew tap phinze/homebrew-cask  # adds tap to '/usr/local/Library/Taps'
+brew tap caskroom/homebrew-cask  # adds tap to '/usr/local/Library/Taps'
 brew install brew-cask         # install the brew Keg.
 # brew upgrade brew-cask       # upgrade the brew-cask Keg from the phinze/homebrew-cask tap.
 # brew cask search partial-name
